5e134b1e0e84e82b1fce1001c82b1e0be9661bf0
Andrew Emmanuel Alonso Valenzuela <andrew.alonso@citi.com.mx>
2025-03-04 13:07:21 -0600

    Actualizacion en Shopping

--- src/main/java/com/alonsocorporation/pointofsale/controllers/ShoppingController.java
+++ src/main/java/com/alonsocorporation/pointofsale/controllers/ShoppingController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/shopping")
+@RequestMapping("/shoppings")
 public class ShoppingController {
 
     private final ShoppingService shoppingService;
--- src/main/java/com/alonsocorporation/pointofsale/dto/response/ShoppingDTO.java
+++ src/main/java/com/alonsocorporation/pointofsale/dto/response/ShoppingDTO.java
@@ -11,7 +11,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 public class ShoppingDTO {
     private Long id;
     private SupplierDTO supplier;
-    private List<ShoppingProductDTO> saleProducts;
+    private List<ShoppingProductDTO> shoppingProducts;
     private Double amount;
     private Double total;
     private final LocalDateTime createdAt;
@@ -19,7 +19,7 @@ public class ShoppingDTO {
     public ShoppingDTO(Shopping shopping) {
         this.id = shopping.getId();
         this.supplier = shopping.getSupplier() != null ? new SupplierDTO(shopping.getSupplier()) : null;
-        this.saleProducts = shopping.getShoppingProducts() != null
+        this.shoppingProducts = shopping.getShoppingProducts() != null
                 ? shopping.getShoppingProducts().stream().map(ShoppingProductDTO::new).toList()
                 : List.of();
         this.amount = shopping.getAmount();
